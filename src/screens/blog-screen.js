d5b353295889ef2c386ffcaec752cc95a56ad7d0
kasuba harish <kasubaharish04@gmail.com>
2024-08-07 21:37:15 +0530

    Hoc counter task done using HoC

--- src/screens/blog-screen.js
+++ src/screens/blog-screen.js
@@ -1,21 +1,34 @@
 import RegistrationForm from "../components/Forms/uncontrolled/controlled/Registration-form";
+import withCounter from "../components/Hoc/CounterHoc";
 import withProfileData from "../components/Hoc/withprofile";
 import NavigationNavBar from "../components/nav-navbar/navigation-navbar";
 import ToggleFunction from "../components/ReactMemo/parent";
 import UseReducer from "../components/useReducer/useReducer";
 
-const  BlogScreen=(props)=>{
+const  BlogScreen=({count,increment,decrement,reset})=>{
     return(
         <>
         <NavigationNavBar/>
+
+
+           <div>
+            <h1>Counter:{count}</h1>
+            <button onClick={increment}> Increment+</button>
+            <button onClick={decrement}> Decrement-</button>
+            <button onClick={reset}> reset</button>
+
+        </div>
         <ToggleFunction/>
       
-        <h1> Well Come Blog Screen</h1>
-        <h3>Good Morning {props.data.username}</h3>
+        {/* <h1> Well Come Blog Screen</h1>
+        <h3>Good Morning {props.data.username}</h3> */}
+
         <UseReducer/>
+
+     
         
        
         </>
     )
 };
-export default withProfileData(BlogScreen);
+export default withCounter(BlogScreen);
